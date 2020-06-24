af9cf5364b5748c15d761598ede5f54f9f294537
OhkiShinomiya <e6akita@yahoo.co.jp>
2020-06-24 17:15:49 +0900

    '1715'

--- test_counter.py
+++ test_counter.py
@@ -1,17 +1,14 @@
-import unittest
+import pytest
 import psycopg2
 import counter  # テスト対象のモジュールをインポートする
 
 
-class test_counter(unittest.TestCase):
-    """helloworld モジュールのテストを記述するクラス"""
-
-    def test_greet(self):
-        counter.ListMaker(0,'foreigner','在来種')
-        amount = counter.GetCount()
-        answer = int(TotalCount()) 
-        # 関数の返り値が期待した内容と一致するか確認する
-        self.assertEqual(amount, answer-2)
+def test_counter():
+    counter.ListMaker(0,'foreigner','在来種')
+    amount = counter.GetCount()
+    answer = int(TotalCount()) 
+    # 関数の返り値が期待した内容と一致するか確認する
+    assert amount == answer-2
 
 def get_connection():
     return psycopg2.connect(host="abunator.postgres.database.azure.com",database="Abunator",user="teamD@abunator",port=5432, password="Nagato1109")
@@ -25,8 +22,3 @@ def TotalCount():
             Maya = i[0]
             break
         return int(Maya)
-
-
-if __name__ == '__main__':
-    # スクリプトとして実行された場合の処理
-    unittest.main()
